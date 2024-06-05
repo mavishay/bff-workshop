4651362fe1a78a053271959d3c4fcd8f14481272
Avishay Maor <avishay@maor.co>
2024-06-05 18:58:04 +0700

    end of step 6 -websocket

--- bff_workshop/main.py
+++ bff_workshop/main.py
@@ -7,6 +7,7 @@ from fastapi import FastAPI
 from fastapi_sqlalchemy import DBSessionMiddleware
 
 from bff_workshop.book.views import router as book_router
+from bff_workshop.questions.views import router as question_router
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 load_dotenv(os.path.join(BASE_DIR, ".env"))
@@ -17,6 +18,7 @@ app = FastAPI()
 app.add_middleware(DBSessionMiddleware, db_url=os.environ["DATABASE_URL"])
 
 app.include_router(book_router, prefix="/book", tags=["Book"])
+app.include_router(question_router, prefix="", tags=["Question"])
 
 @app.get("/health")
 def health():
